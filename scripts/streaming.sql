1e4a1c042200acb4b11cfdb2d998ea0d3843d18b
Miguel Angel López <admon@tecnott.com.mx>
2022-03-23 00:55:26 -0600

    Project of data base with mySQL of movies playlists or series playlists in streaming platforms

---
+++ scripts/streaming.sql
@@ -0,0 +1,111 @@
+CREATE DATABASE IF NOT EXISTS streaming
+DEFAULT {
+CHARACTER SET utf8
+COLLATE utf8_spanish2_ci
+};
+
+USE streaming;
+
+CREATE TABLE classifications(
+id INT NOT NULL AUTO_INCREMENT,
+classification VARCHAR(5) NOT NULL,
+PRIMARY KEY (id)
+)
+{
+COMMENT 'Clasification'
+};
+
+CREATE TABLE movie_genres(
+id INT NOT NULL AUTO_INCREMENT,
+movie_genre VARCHAR(50) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE streaming_services(
+id INT NOT NULL AUTO_INCREMENT,
+streaming_service VARCHAR(15) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE origin_countries(
+id INT NOT NULL AUTO_INCREMENT,
+origin_country VARCHAR(50) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE seasons(
+id INT NOT NULL AUTO_INCREMENT,
+season SMALLINT NOT NULL DEFAULT "1",
+chapters SMALLINT NOT NULL DEFAULT "8",
+PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+id INT NOT NULL AUTO_INCREMENT,
+login VARCHAR(10) NOT NULL,
+pass VARCHAR(10) NOT NULL,
+user_name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id),
+);
+
+CREATE TABLE movies(
+id INT NOT NULL AUTO_INCREMENT,
+movie_name VARCHAR(150) NOT NULL,
+duration TIME NOT NULL,
+movie_year INT NOT NULL,
+streaming_service_id INT NOT NULL,
+movie_genre_id INT NOT NULL,
+classification_id INT NOT NULL,
+origin_country_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (streaming_service_id) REFERENCES streaming_services(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (movie_genre_id) REFERENCES movie_genres(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (classification_id) REFERENCES classifications(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (origin_country_id) REFERENCES origin_countries(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE series(
+id INT NOT NULL AUTO_INCREMENT,
+serie_name VARCHAR(150) NOT NULL,
+seasons SMALLINT NOT NULL DEFAULT "1",
+chapters SMALLINT NOT NULL,
+streaming_service_id INT NOT NULL,
+movie_genre_id INT NOT NULL,
+classification_id INT NOT NULL,
+origin_country_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (streaming_service_id) REFERENCES streaming_services(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (movie_genre_id) REFERENCES movie_genres(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (classification_id) REFERENCES classifications(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (origin_country_id) REFERENCES origin_countries(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE user_movies(
+id INT NOT NULL AUTO_INCREMENT,
+movie_view TINYINT NOT NULL DEFAULT "0",
+seasons SMALLINT NOT NULL DEFAULT "1",
+movie_id INT NOT NULL,
+user_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (movie_id) REFERENCES movies(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE series_seasons(
+id INT NOT NULL AUTO_INCREMENT,
+serie_id INT NOT NULL,
+season_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (serie_id) REFERENCES series(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (season_id) REFERENCES seasons(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE user_series(
+id INT,
+serie_view TINYINT NOT NULL DEFAULT "0",
+serie_season_id INT NOT NULL,
+user_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (serie_season_id) REFERENCES series_seasons(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
